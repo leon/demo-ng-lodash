58f15678950494e93d2b90aaef6d8229c66ba169
Leon Radley <leon@wec360.se>
2017-05-04 08:39:45 +0200

    Two ways of importing lodash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import _ from 'lodash';
+
+import map from 'lodash/map';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+
+  items = [
+    { id: 1 }, 
+    { id: 2 },
+    { id: 3 }
+  ];
+
+  ids: number[];
+  ids2: number[];
+
+  constructor() {
+
+    // Use the global lodash import, this isn't optimal since it will pull in the whole lodash object
+    // But it can be convenient in testing
+  	this.ids = _.map(this.items, 'id');
+    console.log('ids', this.ids);
+
+    // A better way is to import the specific lodash function, that way it's tree shaking friendly
+    this.ids2 = map(this.items, 'id');
+    console.log('ids2', this.ids2);
+  }
 }
